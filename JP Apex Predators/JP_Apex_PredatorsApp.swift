25d3c9687aedeb80509a4de9c17476e2adc210a7
laxxxx <lakshmancool9@gmail.com>
2025-07-14 22:40:54 +0530

    initial commit

---
+++ JP Apex Predators/JP_Apex_PredatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  JP_Apex_PredatorsApp.swift
+//  JP Apex Predators
+//
+//  Created by Sree Lakshman on 14/07/25.
+//
+
+import SwiftUI
+
+@main
+struct JP_Apex_PredatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
